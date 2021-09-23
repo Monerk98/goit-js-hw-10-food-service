cdd6e77398a8ad3fb4bb4b3af45a9a9f77629829
Monerk98 <81251116+Monerk98@users.noreply.github.com>
2021-09-23 14:17:20 +0300

    darkMode

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import menu from './menu.json';
+import cards from './templates/markup.hbs';
+
+//===========================================\\
+
+console.log(cards({menu}))
+
+const main = document.querySelector('.js-menu');
+const menuMarkup = createTheme(menu);
+
+main.insertAdjacentHTML('beforeend', menuMarkup);
+
+function createTheme(menu) {
+  return cards(menu);
+}
+
+//===========================================\\
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+  
+  const checkbox = document.querySelector('#theme-switch-toggle');
+  const light = Theme.LIGHT;
+  const dark = Theme.DARK;
+  
+  checkbox.addEventListener('change', ChangeTheme);
+  
+  function ChangeTheme(e) {
+    e.preventDefault();
+  
+    if (!e.target.checked) {
+      toggleMain(light, dark);
+      localStorage.setItem('theme', light);
+    }
+    else if (e.target.checked) {
+      toggleMain(dark, light);
+      localStorage.setItem('theme', dark);
+    }
+  }
+  
+  function toggleMain(thisColor, futureColor) {
+    document.body.classList.remove(futureColor);
+    document.body.classList.add(thisColor);
+  }
+  
