df1b119216dceaa800cce9afbf4285cd0cb65090
Monerk98 <81251116+Monerk98@users.noreply.github.com>
2021-09-24 20:39:41 +0300

    last

--- src/index.js
+++ src/index.js
@@ -16,31 +16,44 @@ function createTheme(menu) {
 
 //===========================================\\
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
-  
-  const checkbox = document.querySelector('#theme-switch-toggle');
-  const light = Theme.LIGHT;
-  const dark = Theme.DARK;
-  
-  checkbox.addEventListener('change', ChangeTheme);
-  
-  function ChangeTheme(e) {
-    e.preventDefault();
-  
-    if (!e.target.checked) {
-      toggleMain(light, dark);
-      localStorage.setItem('theme', light);
-    }
-    else if (e.target.checked) {
-      toggleMain(dark, light);
-      localStorage.setItem('theme', dark);
-    }
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const checkbox = document.querySelector('#theme-switch-toggle');
+const light = Theme.LIGHT;
+const dark = Theme.DARK;
+let local = localStorage.getItem('theme');
+cache();
+
+checkbox.addEventListener('change', ChangeTheme);
+
+function ChangeTheme(e) {
+  e.preventDefault();
+
+  if (!e.target.checked) {
+    toggleMain(light, dark);
+    localStorage.setItem('theme', light);
   }
-  
+  else if (e.target.checked) {
+    toggleMain(dark, light);
+    localStorage.setItem('theme', dark);
+  }
+}
+
   function toggleMain(thisColor, futureColor) {
     document.body.classList.remove(futureColor);
     document.body.classList.add(thisColor);
   }
-  
+  function cache() {
+    if (local === null) {
+      return;
+    }
+    if (local === light) {
+      document.body.classList.add(light);
+      checkbox.checked = false;
+    } else {
+      document.body.classList.add(dark);
+      checkbox.checked = true;
+    }
+  }
